25940d21860ac9d73cf46ef5181a513a84111568
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-11 02:29:08 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -22,13 +22,13 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  new_cart = consolidate_cart(cart)
+  couped_cart = apply_coupons(new_cart, coupons)
+  total = apply_clearance(couped_cart).sum do |e|
+    (e[:price] * e[:count]).round(2)
+  end
+  if total > 100
+    (total = total *= 0.90).round(2)
+  end
+  total
 end
