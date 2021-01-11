4af1a7bc537fcfbe808591ea717fb03a4b4e25f9
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-11 02:29:48 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,14 +1,14 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-    coupon_cart = []
-    cart.each do |cart_item|
-      coupons.each do |coupon_item|
-       if cart_item[:item] == coupon_item[:item] && cart_item[:count] >= coupon_item[:num]
-        discounted_item = {:item => "#{cart_item[:item]} W/COUPON", :price => coupon_item[:cost] / coupon_item[:num], :clearance => cart_item[:clearance], :count => coupon_item[:num]}
-        cart_item[:count] = cart_item[:count] - coupon_item[:num]
-        if  cart_item[:count] > 0
-          remainder_it
+  coupon_cart = []
+  cart.each do |cart_item|
+    coupons.each do |coupon_item|
+     if cart_item[:item] == coupon_item[:item] && cart_item[:count] >= coupon_item[:num]
+      discounted_item = {:item => "#{cart_item[:item]} W/COUPON", :price => coupon_item[:cost] / coupon_item[:num], :clearance => cart_item[:clearance], :count => coupon_item[:num]}
+      cart_item[:count] = cart_item[:count] - coupon_item[:num]
+      if  cart_item[:count] > 0
+        remainder_it
 end
 
 def apply_clearance(cart)
